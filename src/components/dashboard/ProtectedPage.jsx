479243cb139ce0faeb23243f34ea07af0ae710a8
Jmenzies722 <Jmenzies722@gmail.com>
2023-12-20 14:28:15 -0500

    working on main-content

--- src/components/dashboard/ProtectedPage.jsx
+++ src/components/dashboard/ProtectedPage.jsx
@@ -1,14 +1,14 @@
+import ContentMain from './ContentMain'; // Import ContentMain
 import SideNav from "./sidebar/SideNav"; // Update with the correct path
 import "./ProtectedPage.css"; // Your existing or new CSS file for ProtectedPage
+
 
 function ProtectedPage() {
   return (
     <div className="protected-page">
       <SideNav />
       <div className="dashboard-container">
-        <h1></h1>
-        <h1>Dashboard</h1>
-        <div className="dashboard-content">{/* Your content here */}</div>
+        <ContentMain /> {/* Include ContentMain component here */}
       </div>
     </div>
   );
