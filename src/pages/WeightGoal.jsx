e499637f0e6a5717ef2d0cfb96fc250fabdb231c
Jmenzies722 <Jmenzies722@gmail.com>
2024-01-11 14:18:42 -0500

    updateweightgoalpages

--- src/pages/WeightGoal.jsx
+++ src/pages/WeightGoal.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'; // Import useParams
 import WeightLog from '../components/FeaturePage/WeightLog/WeightLog';
 import SideNav from '../components/dashboard/sidebar/SideNav';
+import UserProfile from '../components/dashboard/UserProfile/UserProfile';
 
 function WeightGoal() {
     const { id } = useParams(); // Extract user ID from URL
@@ -9,6 +10,7 @@ function WeightGoal() {
         <div className="App">
             <WeightLog showInputs={true} />
             <SideNav userId={id} /> {/* Pass the extracted user ID */}
+            <UserProfile/>
         </div>
     );
 }
