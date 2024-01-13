c39569a812369c7cc0c9880c7c359ac2c497cbd2
Jmenzies722 <Jmenzies722@gmail.com>
2024-01-12 22:52:09 -0500

    remove menu nav

--- src/components/dashboard/sidebar/SideNav.jsx
+++ src/components/dashboard/sidebar/SideNav.jsx
@@ -1,19 +1,14 @@
 // SideNav.jsx
-import  { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getSidebarData } from './SidebarData'; // Adjust the import path as needed
 import * as FaIcons from 'react-icons/fa';
-import { IconContext } from 'react-icons';
 import logo from '../../../../assets/zenvibe.png'; // Update the path to your logo
 import './SideNav.css';
 
 function SideNav({ userId }) {
     const navigate = useNavigate();
-    const [sidebar, setSidebar] = useState(true);
     const sidebarData = getSidebarData(userId);
-
-    const showSidebar = () => setSidebar(!sidebar);
 
     const handleLogout = async () => {
         try {
@@ -29,35 +24,28 @@ function SideNav({ userId }) {
     };
 
     return (
-        <IconContext.Provider value={{ color: '#fff' }}>
-            <div className='sidenav'>
-                <Link to='#' className='menu-bars'>
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items' onClick={showSidebar}>
-                    <div className='nav-menu-header'>
-                        <img src={logo} alt='ZenVibe Logo' className='sidebar-logo' />
-                        <h2>ZenVibe</h2>
-                    </div>
-                    {sidebarData.map((item, index) => (
-                        <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span>{item.title}</span>
-                            </Link>
-                        </li>
-                    ))}
-                    <li className='nav-text'>
-                        <Link to='#' onClick={handleLogout}>
-                            <FaIcons.FaSignOutAlt />
-                            <span>Sign Out</span>
+        <nav className='nav-menu active'> {/* Always active */}
+            <ul className='nav-menu-items'>
+                <div className='nav-menu-header'>
+                    <img src={logo} alt='ZenVibe Logo' className='sidebar-logo' />
+                    <h2>ZenVibe</h2>
+                </div>
+                {sidebarData.map((item, index) => (
+                    <li key={index} className={item.cName}>
+                        <Link to={item.path}>
+                            {item.icon}
+                            <span>{item.title}</span>
                         </Link>
                     </li>
-                </ul>
-            </nav>
-        </IconContext.Provider>
+                ))}
+                <li className='nav-text'>
+                    <Link to='#' onClick={handleLogout}>
+                        <FaIcons.FaSignOutAlt />
+                        <span>Sign Out</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
