859c01679e54331980584467caf6222632c06173
Jmenzies722 <Jmenzies722@gmail.com>
2024-01-22 11:39:58 -0500

    updated calorie calculation

--- src/components/FeaturePage/Food/bmrCalculator.jsx
+++
@@ -1,6 +0,0 @@
-export const calculateBMR = (gender, weight, height, age) => {
-    return gender === 'Female'
-      ? Math.floor(655.1 + (4.35 * weight) + (4.7 * height) - (4.676 * age))
-      : Math.floor(66.47 + (6.24 * weight) + (12.7 * height) - (6.755 * age))
-  };
-  
