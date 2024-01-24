75cd1d173c507aa660b599509ad79fe2c6e22edf
Jmenzies722 <Jmenzies722@gmail.com>
2024-01-23 23:19:32 -0500

    updated mood-tracker

--- src/pages/MoodTracker.jsx
+++ src/pages/MoodTracker.jsx
@@ -1,4 +1,5 @@
 import MoodLog from "../components/FeaturePage/MoodTracker/MoodLog";
+import UserProfile from "../components/dashboard/UserProfile/UserProfile";
 import SideNav from "../components/dashboard/sidebar/SideNav";
 import { useParams } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function MoodTracker() {
         <div className="App">
             <SideNav userId={userId}/>
             <MoodLog userId={userId}/>
+            <UserProfile/>
         </div>
     );
 }
